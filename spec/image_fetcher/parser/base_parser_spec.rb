a6842b3d560d1ad6f4a0ef3f4566748f8160ad02
Egor Gorodov <egor.gorodov@gmail.com>
2015-07-16 12:14:26 +0300

    fix base_parser spec

--- spec/image_fetcher/parser/base_parser_spec.rb
+++ spec/image_fetcher/parser/base_parser_spec.rb
@@ -1,22 +1,20 @@
 require 'spec_helper'
 
-describe ImageFetcher::ImageUrlsParser do
+describe ImageFetcher::Parser::BaseParser do
   let(:good_url_with_images) { "http://familyguy.fox-fan.ru" }
   let(:good_url_without_images) { "http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html" }
 
   context 'when url is good' do
     context 'when html page has images' do
       it 'returns image urls' do
-        parser = ImageFetcher::ImageUrlsParser.new(good_url_with_images)
-        image_urls = parser.get_image_urls
-        expect(image_urls).not_to be_empty
+        image_infos = ImageFetcher::Parser::BaseParser.parse(good_url_with_images)
+        expect(image_infos).not_to be_empty
       end
 
       context "when html page hasn't images" do
         it 'return empty array' do
-          parser = ImageFetcher::ImageUrlsParser.new(good_url_without_images)
-          image_urls = parser.get_image_urls
-          expect(image_urls).to be_empty
+          image_infos = ImageFetcher::Parser::BaseParser.parse(good_url_without_images)
+          expect(image_infos).to be_empty
         end
       end
     end
