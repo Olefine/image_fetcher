009a388139ac154448670a102e9e98f7dbf6dc29
Egor Gorodov <egor.gorodov@gmail.com>
2015-07-19 16:14:24 +0300

    wrap basic ruby logger into singleton abstraction

--- lib/image_fetcher/logger.rb
+++ lib/image_fetcher/logger.rb
@@ -1,25 +1,21 @@
+require 'logger'
+
 module ImageFetcher
   class Logger
     class << self
       def init_logger(logfile)
         if File.exist?(logfile)
-          @@file = logfile
+          @@logger ||= ::Logger.new(logfile)
         else
           raise "File doesnt exist"
         end
       end
 
       def log(message)
-        build_logline(message)
-        File.open(@@file, 'a') { |log| log.write(build_logline(message)) }
+        @@logger.add(::Logger::ERROR, message)
       end
 
       private :new
-
-      private
-      def build_logline(message)
-        "#{Time.now} : #{message} \n"
-      end
     end
   end
 end
