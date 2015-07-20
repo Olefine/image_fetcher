2fde3092ddcc777e7444415307c7b3512819935d
Egor Gorodov <egor.gorodov@gmail.com>
2015-07-20 13:19:43 +0300

    replace @@ to @ in logger

--- lib/image_fetcher/logger.rb
+++ lib/image_fetcher/logger.rb
@@ -5,14 +5,14 @@ module ImageFetcher
     class << self
       def init_logger(logfile)
         if File.exist?(logfile)
-          @@logger ||= ::Logger.new(logfile)
+          @logger ||= ::Logger.new(logfile)
         else
           raise "File doesnt exist"
         end
       end
 
       def log(message)
-        @@logger.add(::Logger::ERROR, message)
+        @logger.add(::Logger::ERROR, message)
       end
 
       private :new
